dc4b0537e4c3d6a25d9b48c0e00575d93a7c22b4
Sam Jp <samratjp@gmail.com>
2016-09-19 17:03:12 -0700

    added access token extension to fb calls

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,19 +1,26 @@
 module SocialShares
   class Facebook < Base
-    URL = 'http://graph.facebook.com/v2.7/'
+    URL = 'https://graph.facebook.com/v2.7/'
 
     def shares!
-      response = get(URL, params: {
-        id: checked_url,
-        fields: 'share'
-      })
+      response = get(URL, params: params)
       json_response = JSON.parse(response)
-
+      sum = 0
       if json_response['share']
-        json_response['share']['share_count'] || 0
-      else
-        0
+        json_response['share'].each do |share_type, count|
+          sum += count
+        end
       end
+      sum
+    end
+
+    def params
+      _params = { id: checked_url, fields: 'share' }
+      token = ENV['FACEBOOK_ACCESS_TOKEN']
+      if token 
+        _params.merge!(access_token: token)        
+      end
+      _params
     end
   end
 end
