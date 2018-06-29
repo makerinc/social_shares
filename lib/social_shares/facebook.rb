a295afc726f29134ea8c021598be85595c338f93
Sam Jp <samratjp@gmail.com>
2018-06-28 22:12:55 -0700

    use latest fb api version for share count

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,24 +1,24 @@
 module SocialShares
   class Facebook < Base
-    URL = 'https://graph.facebook.com/v2.7/'
+    URL = 'https://graph.facebook.com/v3.0/'
 
     def shares!
       response = get(URL, params: params)
       json_response = JSON.parse(response)
       sum = 0
-      if json_response['share']
-        json_response['share'].each do |share_type, count|
-          sum += count
+      if json_response['engagement']
+        json_response['engagement'].each do |share_type, count|
+          sum += count unless share_type == 'comment_plugin_count'
         end
       end
       sum
     end
 
     def params
-      _params = { id: checked_url, fields: 'share' }
+      _params = { id: checked_url, fields: 'reaction' }
       token = ENV['FACEBOOK_ACCESS_TOKEN']
-      if token 
-        _params.merge!(access_token: token)        
+      if token
+        _params.merge!(access_token: token)
       end
       _params
     end
